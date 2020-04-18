217a244a3519d46d5bb2641223387d69aa26adf3
oct88oct <chinstermojo@gmail.com>
2020-04-18 10:28:22 +0800

    Before changing HTML

---
+++ contracts/CoinFlip.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.21;
+
+contract CoinFlip {
+    address public player1;
+    uint256 public player1Password;
+
+    uint256 public betAmount;
+
+    address public player2;
+    bool public player2Choice;
+
+    uint256 public expiration = 2**256-1;
+
+    function CoinFlip(uint256 Password) public payable {
+        player1 = msg.sender;
+        player1Password = Password;
+        betAmount = msg.value;
+    }
+
+    function cancel() public {
+        require(msg.sender == player1);
+        require(player2 == 0);
+
+        betAmount = 0;
+        msg.sender.transfer(address(this).balance);
+    }
+
+    function takeBet(bool choice) public payable {
+        require(player2 == 0);
+        require(msg.value == betAmount);
+
+        player2 = msg.sender;
+        player2Choice = choice;
+
+        expiration = now + 24 hours;
+    }
+
+    function reveal(bool choice, uint256 Password) public {
+        require(player2 != 0);
+        require(now < expiration);
+
+        require(Password == player1Password);
+
+        if (player2Choice == choice) {
+            player2.transfer(address(this).balance);
+        } else {
+            player1.transfer(address(this).balance);
+        }
+    }
+
+    function claimTimeout() public {
+        require(now >= expiration);
+
+        player2.transfer(address(this).balance);
+    }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -4,11 +4,17 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 5777,
       network_id: "*" // Match any network id
     },
     develop: {
       port: 8545
     }
-  }
+    
+  },
+  compilers: {
+    solc: {
+      version: "0.4.21",
+    },
+  },
 };
